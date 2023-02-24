24b56e61f16896f690bd2661062b12813d2d1644
amirziarati <amir.ziarati@theScore.com>
2023-02-24 13:27:55 -0500

    removed extra files

--- app/src/main/java/com/example/tsbmock/MainActivity.kt
+++ app/src/main/java/com/example/tsbmock/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.tsbmock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.sharedmock.TheData
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.tvValue).text = TheData.value
     }
 }
